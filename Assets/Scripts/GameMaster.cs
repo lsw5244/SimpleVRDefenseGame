5073b005d9873e03ae3a98d9e5903c3adba7134c
LEW <lsw5244@naver.com>
2020-06-11 17:06:22 +0900

    Unity 2019.3.4f1 버전으로 생성, 오브젝트 임포트. LaserPointer 작성
    버전 2019.3.4f1버전으로 생성
    맵, 몬스터 오브젝트 임포트 및 맵 제작 및 카메라 배치
    LaserPointer스크립트로 레이캐스트 보이도록제작
    LaserPointer스크립트로 버튼 클릭 구현
    버튼 클릭 했을 땐 GameMaster의 메소드 실행

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void StartButtonClick()
+    {
+        Debug.Log("START BTN CLICK@@@@@@@@@@@@@@@@@@@@");
+    }
+
+    public void GameRestartButtonClick()
+    {
+        Debug.Log("RESET@@@@@@@@@@@@@@@@@@@@@");
+    }
+}
---
+++ Assets/Scripts/LaserPointer.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using UnityEngine.EventSystems;
+
+public class LaserPointer : MonoBehaviour
+{
+    private SteamVR_Behaviour_Pose pose;
+    private SteamVR_Input_Sources hand;
+    private LineRenderer line;
+
+    public SteamVR_Action_Boolean trigger = SteamVR_Actions.default_InteractUI;  //트리거
+
+    public float maxDistance = 20.0f; //라인의 최대 거리
+
+    // 레이캐스트를 위한 변수 선언
+    private RaycastHit hit;
+    // 컨트롤러의 Transfrom 컴포넌트를 저장 할 변수
+    private Transform tr;
+
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        pose = GetComponent<SteamVR_Behaviour_Pose>();
+        hand = pose.inputSource;
+        CreateLineRenderer();
+
+    }
+
+    void Update()
+    {
+        if (Physics.Raycast(tr.position, tr.forward, out hit, maxDistance))
+        {
+            // 라인의 끝점의 위치를 레이캐스팅한 지점의 좌표로 변경
+            line.SetPosition(1, new Vector3(0, 0, hit.distance));
+
+
+            // 트리거 버튼을 클릭했을 경우에 클릭 이벤트를 발생시킴
+            if (trigger.GetStateDown(hand))
+            {
+                ExecuteEvents.Execute(hit.collider.gameObject
+                    , new PointerEventData(EventSystem.current)
+                    , ExecuteEvents.pointerClickHandler);
+
+                line.material.color = Color.gray;
+            }
+            if(trigger.GetStateUp(hand))
+            {
+                line.material.color = Color.black;
+            }
+
+        }
+        else  //충돌 안했을 때
+        {
+            line.SetPosition(1, new Vector3(0, 0, maxDistance));
+            line.material.color = Color.black;
+        }
+    }
+
+
+
+    private void CreateLineRenderer()
+    {
+        // LineRender생성
+        line = this.gameObject.AddComponent<LineRenderer>();
+        line.useWorldSpace = false;
+        line.receiveShadows = false;
+
+        // 시작과 끝점의 위치 설정
+        line.positionCount = 2;
+        line.SetPosition(0, Vector3.zero);
+        line.SetPosition(1, new Vector3(0, 0, maxDistance));
+
+        // 라인의 너비 설정
+        line.startWidth = 0.03f;
+        line.endWidth = 0.005f;
+
+        // 라인의 머터리얼 및 색상 설정
+        line.material = new Material(Shader.Find("Unlit/Color"));
+        line.material.color = Color.black;
+    }
+}
