3279fbb29fb0775b0edc2d7529631a61284f696b
LEW <lsw5244@naver.com>
2020-06-18 16:42:50 +0900

    바닥에 떨어지면 그냥 사라지도록 변경

--- Assets/Scripts/AttackObject.cs
+++ Assets/Scripts/AttackObject.cs
@@ -6,6 +6,7 @@ public class AttackObject : MonoBehaviour
 {
     GameMaster GM;
     Vector3 originPos;
+    bool isAttack = false;
 
     private void Start()
     {
@@ -17,18 +18,39 @@ public class AttackObject : MonoBehaviour
 
     private void OnCollisionEnter(Collision coll)
     {
+        // 테이블일 경우 리턴
+        if(coll.gameObject.CompareTag("Table"))
+        {
+            return;
+        }
+
         // 몬스터 확인
         if(coll.gameObject.CompareTag("Monster"))
         {
-            // 몬스터 제거
-            Destroy(coll.gameObject);
+            // 겹처서 공격 안되도록 함
+            if (!isAttack)
+            {             
+                isAttack = true;
 
-            // 점수 올리기
-            GM.score += 100;
+                // 몬스터 제거
+                Destroy(coll.gameObject);
 
-            // 몬스터 카운트 줄이기
-            GM.monsterCount -= 1;
+                // 점수 올리기
+                GM.score += 100;
 
+                // 몬스터 카운트 줄이기
+                GM.monsterCount -= 1;
+
+                // 다른 오브젝트 재생성
+                GM.RespawnAttackObject(originPos);
+
+                // 본인 제거
+                Destroy(this.gameObject);
+            }
+        }
+        // 바닥에 그냥 떨어졌을 때
+        if (coll.gameObject.CompareTag("Ground"))
+        {
             // 다른 오브젝트 재생성
             GM.RespawnAttackObject(originPos);
 
